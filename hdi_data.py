9ca01510d529a80afb55f9b705a2a08e35a0098e
Kelsie Claunch <89403622+kelsieclaunch@users.noreply.github.com>
2025-09-04 00:47:31 -0700

    remove db files, add csv
    added in checks for changes to the product list

--- hdi_data.py
+++ hdi_data.py
@@ -1,8 +1,7 @@
 
 import requests
-import json
-import dataset
-import sqlite3
+import csv
+import os
 
 class ShopifyScraper():
 
@@ -13,11 +12,27 @@ class ShopifyScraper():
         r = requests.get(self.baseurl + f'products.json?limit=250&page={page}', timeout=5)
         if r.status_code != 200:
             print('Bad status code: ' + str(r.status_code))
-        if len(r.json()['products']) > 0:
-            data = r.json()['products']
-            return data
+            return None
+        
+        data = r.json().get('products', [])
+        return data if data else None
+    
+    def normalize_size(self, size_str):
+        """Normalize size names to S, M, L, XL, etc."""
+        size_str = size_str.lower()
+        if 'small' in size_str:
+            return 'S'
+        elif 'medium' in size_str:
+            return 'M'
+        elif 'large' in size_str and 'x' not in size_str:
+            return 'L'
+        elif 'x-large' in size_str or 'extra large' in size_str:
+            return 'XL'
+        elif 'xx-large' in size_str or '2x' in size_str:
+            return 'XXL'
         else:
-            return
+            return '' # leave blank if no match
+        
         
     def parse_json(self, jsondata):
         products = []
@@ -30,10 +45,15 @@ class ShopifyScraper():
             for v in prod['variants']:
                 varid = str(v['id'])
                 var_link = main_link + '?variant=' + varid
+
+
+                size_raw = v['title']
+                size_normalized = self.normalize_size(size_raw)
+
                 item = {
                     'title': main_title,
                     'type' : main_type,
-                    'variant': v['title'],
+                    'size': size_normalized,
                     'v_id': v['id'],
                     'available': v['available'],
                     'price': v['price'],
@@ -45,28 +65,82 @@ class ShopifyScraper():
                 
         return products
     
+
+
+def save_to_csv(products, filename='hiidef_products.csv'):
+    if not products:
+        print("No products to save.")
+        return
+
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    filepath = os.path.join(script_dir, filename)
+
+    # Load existing data (if file exists)
+    existing_products = {}
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                existing_products[row['v_id']] = row
+
+    for p in products:
+        vid = str(p['v_id'])
+
+        if vid not in existing_products:
+            # TODO: TWEET NEW PRODUCT ADDED
+
+            pass
+
+        else:
+            old = existing_products[vid]
+            old_available = old['available']
+            new_available = p['available']
+
+            if old_available != new_available:
+                if old_available == True and new_available == False:
+                    #TODO: TWEET SOLD OUT
+                    pass
+                else:
+                    #TODO: TWEET PRODUCT RESTOCKED
+                    pass
+
+
+    keys = products[0].keys()
+    with open(filepath, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(products)
+    
+    print(f"Saved {len(products)} products to {filepath}")
+
+
+        
 def main():
     hiidef = ShopifyScraper('https://hiidef.xyz/')
     results = []
     for page in range(1, 10):
         data = hiidef.download_json(page)
         print('Getting page ', page)
-        try:
-            results.append(hiidef.parse_json(data))
-        except:
+        if not data:
             print(f'Completed, total pages = {page - 1}' )
             break
+        parsed = hiidef.parse_json(data)
+        results.extend(parsed)
     return results
 
-if __name__ == '__main__':
- 
-    db = dataset.connect('sqlite:///hdi-data.db')
-    table = db.create_table('hiidef', primary_id='v_id')
-    products = main()
-    totals = [item for i in products for item in i] 
-    print(len(totals))
 
-    for p in totals:
-        if not table.find_one(v_id = p['v_id']):
-            table.insert(p)
-            print('NEW PRODUCT: ', p)
+if __name__ == '__main__':
+    products = main()
+    save_to_csv(products)
+ 
+    # db = dataset.connect('sqlite:///hdi-data.db')
+    # table = db.create_table('hiidef', primary_id='v_id')
+    # products = main()
+    # totals = [item for i in products for item in i] 
+    # print(len(totals))
+
+    # for p in totals:
+    #     if not table.find_one(v_id = p['v_id']):
+    #         table.insert(p)
+    #         print('NEW PRODUCT: ', p)
