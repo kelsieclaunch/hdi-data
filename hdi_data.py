f645bfa0d60387fa9716711e26db420ba5fb1f03
Kelsie Claunch <89403622+kelsieclaunch@users.noreply.github.com>
2026-01-05 21:43:30 -0800

    Update debug flag

--- hdi_data.py
+++ hdi_data.py
@@ -473,7 +473,7 @@ def run_job():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 
